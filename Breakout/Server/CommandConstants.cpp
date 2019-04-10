044cafff59c6dfe1c2c6eb1ea2c8cc0142981c99
Skully <Wallace9655@gmail.com>
2019-04-10 05:25:05 +0100

    Added Constants for commands

---
+++ Breakout/Server/CommandConstants.cpp
@@ -0,0 +1,10 @@
+#include "CommandConstants.h"
+
+const tstring CommandConstants::bonusDropRate	= TEXT("chance_bonus");
+const tstring CommandConstants::levelCount		= TEXT("nlevels");
+const tstring CommandConstants::maxLives		= TEXT("vidas_ini");
+const tstring CommandConstants::movementSpeed	= TEXT("velocidade_movimento");
+const tstring CommandConstants::playerCount		= TEXT("max_jogadores");
+const tstring CommandConstants::slowDown		= TEXT("slowdowns");
+const tstring CommandConstants::speedUp			= TEXT("speedups");
+const tstring CommandConstants::tileCount		= TEXT("tijolos_iniciais");
