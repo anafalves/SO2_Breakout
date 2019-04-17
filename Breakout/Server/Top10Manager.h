77c78ee045f4fc2fb7da96f37110145b8f1ab1bb
Skully <Wallace9655@gmail.com>
2019-04-17 08:07:53 +0100

    Added top10 manager class, currently bugged with [conversion to binary not working properly]

---
+++ Breakout/Server/Top10Manager.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <Windows.h>
+#include <tchar.h>
+#include <string>
+#include <sstream>
+
+#include "GameData.h"
+#include "UnicodeConfigs.h"
+
+using namespace std;
+
+typedef struct {
+	TCHAR username[50];
+	int points;
+}Place;
+
+typedef struct {
+	Place position[10];
+}Top10;
+
+
+class Top10Manager
+{
+private:
+	Top10 top10;
+
+public:
+	Top10Manager();
+	~Top10Manager();
+
+	void addPlayer(Player & player);
+
+	Top10 getTop10() const {
+		return top10;
+	}
+
+	tstring getAsString();
+};
+
