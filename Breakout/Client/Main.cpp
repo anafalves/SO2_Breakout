aaa894aba21c49ef7565b12df6c2b7a972bd9805
Skully <Wallace9655@gmail.com>
2019-05-03 20:28:21 +0100

    Changed client main to make it so that new local users won't log into the server

--- Breakout/Client/Main.cpp
+++ Breakout/Client/Main.cpp
@@ -20,19 +20,28 @@ DWORD WINAPI receiveBallUpdates() {
 }
 
 int _tmain(int argc, TCHAR ** argv) {
-	
 
 	if (argc != 2) {
 		return -1;
 	}
 
+	HANDLE alreadyRunning = INVALID_HANDLE_VALUE;
 	CONTINUE = true;
 	client = getClientInstance();
+	
+	alreadyRunning = CreateEvent(NULL,TRUE,FALSE, TEXT("LocalClientRunning"));
+	if (alreadyRunning != INVALID_HANDLE_VALUE && GetLastError() == ERROR_ALREADY_EXISTS) {
+		tcout << "A local client is already running, you will join as espectator!" << endl;
+		thread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)receiveBallUpdates, NULL, 0, NULL);
+	}
+	else 
+	{
+		//TODO: create a class to start this up
 
-	if (client->login(argv[1])) {
-		tcout << "login accepted" << endl;
-		
-		thread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE) receiveBallUpdates, NULL, 0, NULL);
+		if (client->login(argv[1])) {
+			tcout << "login accepted" << endl;
+			thread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)receiveBallUpdates, NULL, 0, NULL);
+		}
 	}
 
 	_gettchar();
